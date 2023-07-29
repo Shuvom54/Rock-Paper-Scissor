e1b7a712c340124efade57eebb69fe8be33f2c13
Shuvom54 <140581655+Shuvom54@users.noreply.github.com>
2023-07-29 22:19:14 +0530

    Update script.js

--- RockPaperScissors/script.js
+++ RockPaperScissors/script.js
@@ -1,18 +1,6 @@
-/*
-  Rock Paper Scissors 🚀🔥
-  Concepts covered in this project
-    👉 For loops
-    👉 Dom Manipulation
-    👉 Variables
-    👉 Conditionals (if else if)
-    👉 Template Literals
-    👉 Event Listeners
-    👉 Higher order Function (Math.random())
-*/
+
 const totalScore={computerScore:0,playerScore:0};
-// ** getComputerChoice randomly selects between `rock` `paper` `scissors` and returns that string **
-// getComputerChoice() 👉 'Rock'
-// getComputerChoice() 👉 'Scissors'
+
 function getComputerChoice() {
   const computerChoice=["Rock","Paper","Scissors"]
   const randomNumber=Math.floor(Math.random()*3);
@@ -20,13 +8,9 @@ function getComputerChoice() {
 }
 
 
-// ** getResult compares playerChoice & computerChoice and returns the score accordingly **
-// human wins - getResult('Rock', 'Scissors') 👉 1
-// human loses - getResult('Scissors', 'Rock') 👉 -1
-// human draws - getResult('Rock', 'Rock') 👉 0
+
 function getResult(playerChoice, computerChoice) {
-  // return the result of score based on if you won, drew, or lost
-  // All situations where human draws, set `score` to 0
+  
   let score;
   if(playerChoice==computerChoice){
     score=0;
@@ -46,13 +30,8 @@ else if(playerChoice=="Rock"&&computerChoice=="Scissors"){
   // return score
   return score;
 }
-// console.log(getResult("Scissor", "Rock"))
-
-// ** showResult updates the DOM to `You Win!` or `You Lose!` or `It's a Draw!` based on the score. Also shows Player Choice vs. Computer Choice**
 function showResult(score, playerChoice, computerChoice,totalScore) {
-  // Hint: on a score of -1
-  // You should do result.innerText = 'You Lose!'
-  // Don't forget to grab the div with the 'result' id!
+ 
   const resultDiv=document.getElementById('result');
   const handsDiv=document.getElementById('hands');
   const playerScoreId=document.getElementById('player-score');
@@ -80,29 +59,22 @@ function onClickRPS(playerChoice) {
 }
 
 
-// ** Make the RPS buttons actively listen for a click and do something once a click is detected **
+
 function playGame() {
-  // use querySelector to select all RPS Buttons
+
    const rpsButtons=document.querySelectorAll('.rpsButton')
   rpsButtons[0].onclick=()=>console.log(rpsButtons[0].value)
-  // * Adds an on click event listener to each RPS button and every time you click it, it calls the onClickRPS function with the RPS button that was last clicked *
   
-  // 1. loop through the buttons using a forEach loop
-  // 2. Add a 'click' event listener to each button
-  // 3. Call the onClickRPS function every time someone clicks
-  // 4. Make sure to pass the currently selected rps button as an argument
    rpsButtons.forEach(rpsButton=>{
      rpsButton.onclick=()=>onClickRPS(rpsButton.value)
    })
  
 
-  // Add a click listener to the end game button that runs the endGame() function on click
   const endGameButton=document.getElementById('endGameButton');
   endGameButton.onclick=()=>endGame(totalScore);
   
 }
 
-// ** endGame function clears all the text on the DOM **
 function endGame(totalScore) {
   totalScore['playerScore']=0;
   totalScore['ComputerScore']=0;
